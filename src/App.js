2a650138d35c7816ddefe61fb93e9316610be71e
Alessio Cimarelli <jenkin@dataninja.it>
2021-01-13 09:04:02 +0100

    Fix velocity calculation

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ function App () {
   const fmtPerc = numberLoc.format('.0%')
 
   const timeLoc = timeFormatLocale(timeItIT)
-  const fmtTime = timeLoc.format('%A %d %B %Y')
+  const fmtDate = timeLoc.format('%A %d %B %Y')
+  const fmtISODate = timeLoc.format('%Y-%M-%d')
 
   const [data, setData] = useState([])
+  const [area, setArea] = useState('ITA')
   const [lastUpdate, setLastUpdate] = useState(new Date())
   const [lastDays, setLastDays] = useState(7)
   const [lastDate, setLastDate] = useState(new Date())
@@ -38,7 +40,7 @@ function App () {
     window.fetch('./somministrazioni-vaccini-summary-latest.json')
       .then(res => res.json())
       .then(res => res.data)
-      .then(data => data.filter(d => d.area !== 'ITA'))
+      .then(data => data.filter(d => d.area === 'ITA'))
       .then(data => data.sort((a, b) => descending(a.data_somministrazione, b.data_somministrazione)))
       .then(data => { setData(data) })
   }, [])
@@ -48,7 +50,7 @@ function App () {
       rollups(
         data,
         v => sum(v, d => d.totale),
-        d => d.data_somministrazione
+        d => fmtISODate(new Date(d.data_somministrazione))
       )
     )
 
@@ -60,7 +62,7 @@ function App () {
   useEffect(() => {
     setAvgAdministrationsLastDays(
       sum(
-        administrationsPerDay.slice(0, lastDays),
+        administrationsPerDay.slice(1, lastDays + 1),
         d => d[1]
       ) / lastDays
     )
@@ -83,10 +85,10 @@ function App () {
   return (
     <div className='App'>
       <p>Termine previsto della campagna vaccinale italiana contro <em>Sars-CoV-2</em>.</p>
-      <h1>{fmtTime(lastDate)}</h1>
+      <h1>{fmtDate(lastDate)}</h1>
       <p>
         L'Italia ha iniziato a somministrare il primo vaccino approvato dall'EMA e dall'AIFA il <em>27 dicembre 2020</em>.
-        A oggi, <em>{fmtTime(lastUpdate).toLowerCase()}</em>, sono state somministrate <em>{fmtInt(administrations)} dosi</em> a livello nazionale,
+        A oggi, <em>{fmtDate(lastUpdate).toLowerCase()}</em>, sono state somministrate <em>{fmtInt(administrations)} dosi</em> a livello nazionale,
         ne mancano <em>{fmtInt(remainingAdministrations)}</em> prima di riuscire a vaccinare l'<em>{fmtPerc(populationFraction)}</em> della popolazione italiana
         con <em>{fmtInt(doses)} dosi</em> a testa.
         Al ritmo di <em>{fmtInt(avgAdministrationsLastDays)} dosi</em> somministrate al giorno tenuto negli ultimi <em>{fmtInt(lastDays)} giorni</em>,
